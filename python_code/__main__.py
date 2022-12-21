1badf38bca5eebd0f03399863b404acd5dcf3c5c
ziwonjeong <jj970117@naver.com>
2022-12-21 15:11:13 +0900

    add refactor2

--- python_code/__main__.py
+++ python_code/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function, absolute_import
-from .good import __all__ as all_submodules
+from good import __all__ as all_submodules
 import sys
 
 def main(argv):
